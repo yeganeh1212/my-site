76a1bad21846f849e15b8128e4f17e9262eb41a6
yeganeh1212 <bahary.yeganeh@gmail.com>
2024-07-12 16:38:53 +0330

    add render to views and html files to template

--- website/views.py
+++ website/views.py
@@ -5,10 +5,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def index_view(request):
-    return HttpResponse('<h1>Home Page <h1>')
+    return render(request, 'index.html')
 
 def about_view(request):
-    return JsonResponse({'name' : 'about'})
+    return render(request,'about.html')
 
 def contact_view(request):
-    return JsonResponse({'name' : 'contac'})
+    return render(request,'contact.html')
