83389e581e7288e6a2e94dc1d20293efabacefa1
yeganeh1212 <bahary.yeganeh@gmail.com>
2024-07-25 14:05:11 +0330

    some templates and apps added

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+
+
+from django.contrib import admin
+from django.urls import path
+from blog.views import *
+
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('blog', blog_view, name='index'),
+    path('blog/single', blog_single, name='single'),
+
+
+]
--- website/views.py
+++ website/views.py
@@ -5,10 +5,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 
 def about_view(request):
-    return render(request,'about.html')
+    return render(request,'website/about.html')
 
 def contact_view(request):
-    return render(request,'contact.html')
+    return render(request,'website/contact.html')
